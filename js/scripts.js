180d7316af8ef508b540f320a98fa659d89fbbc3
Nikki Boyd and Ryan Putman <nikki@>
2018-05-31 15:42:04 -0700

    add values for question1

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(event) {
+  $("form#quizquestions").submit(function(event) {
+  var question1 = $("#question1choice1").val();
+  var question2 = $(".question2").val();
+  var question3 = $(".question3").val();
+  var question4 = $(".question4").val();
+  var question5 = $(".question5").val();
+  event.preventDefault();
+  });
+});
