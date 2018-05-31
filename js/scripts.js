625586019fd322038a8b2422cb720454ae1ba283
Nikki Boyd and Ryan Putman <ryan@>
2018-05-31 16:43:16 -0700

    add new way to add points

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,36 @@
-function add(question1, question2, question3, question4, question5) {
-  return question1 + question2 + question3 + question4 + question5;
-};
+// function add(question1, question2, question3, question4, question5) {
+//   return question1 + question2 + question3 + question4 + question5;
+// };
 
 $(document).ready(function(event) {
   $("form#quizquestions").submit(function(event) {
-  var question1 = parseInt($("input:radio[name=question1]:checked").val());
-  var question2 = parseInt($("input:radio[name=question2]:checked").val());
-  var question3 = parseInt($("input:radio[name=question3]:checked").val());
-  var question4 = parseInt($("input:radio[name=question4]:checked").val());
-  var question5 = parseInt($("input:radio[name=question5]:checked").val());
+  // var question1 = parseInt($("input:radio[name=question1]:checked").val());
+  // var question2 = parseInt($("input:radio[name=question2]:checked").val());
+  // var question3 = parseInt($("input:radio[name=question3]:checked").val());
+  // var question4 = parseInt($("input:radio[name=question4]:checked").val());
+  // var question5 = parseInt($("input:radio[name=question5]:checked").val());
 
-  if (add(question1, question2, question3, question4, question5) >= 9) {
+  var points = parseInt($("input:radio[name=question1]:checked").val()) + parseInt($("input:radio[name=question2]:checked").val()) + parseInt($("input:radio[name=question3]:checked").val()) + parseInt($("input:radio[name=question4]:checked").val()) + parseInt($("input:radio[name=question5]:checked").val())
+
+  if (points >= 9) {
     $('.result').text("Margot Tenenbaum");
-  } else if (add(question1, question2, question3, question4, question5) >= 6 ) {
+  } else if (points >= 6 ) {
     $('.result').text("Klaus Daimler");
-  } else if (add(question1, question2, question3, question4, question5) === 5) {
+  } else if (points === 5) {
     $('.result').text("Mr. Fox");
   } else {
     $('.result').text("Answer all the questions");;
   }
+
+  // if (add(question1, question2, question3, question4, question5) >= 9) {
+  //   $('.result').text("Margot Tenenbaum");
+  // } else if (add(question1, question2, question3, question4, question5) >= 6 ) {
+  //   $('.result').text("Klaus Daimler");
+  // } else if (add(question1, question2, question3, question4, question5) === 5) {
+  //   $('.result').text("Mr. Fox");
+  // } else {
+  //   $('.result').text("Answer all the questions");;
+  // }
 
   event.preventDefault();
   });
